84e66f3753d747d42b224415d4c7e174d0c2ac9f
karimk123 <64856262+karimk123@users.noreply.github.com>
2020-12-29 19:08:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
-import setuptools
+from distutils.core import setup
+setup(
+  name = 'pccontrol',         # How you named your package folder (MyLib)
+  packages = ['pccontrol'],   # Chose the same as "name"
+  version = '0.1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'This is a python library that gives you functions for doing multiple things on pc.',   # Give a short description about your library
+  author = 'kar1ms',                   # Type in your name
+  author_email = 'tomkaka125@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/karimk123/pccontroller',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/karimk123/pccontroller/archive/v_01.tar.gz',    # I explain this later on
+  keywords = ['pccontroller', 'control pc', 'pc controller'],   # Keywords that define your package best
+  install_requires=[            # I get to this in a second
 
-
-
-setuptools.setup(
-name="pccontroller",
-version="1.0",
-author="kar1ms",
-author_email="tomkaka125@gmail.com",
-description="This is a python library that gives you functions for doing multiple things on pc.",
-url="https://github.com/karimk123/pccontroller",
-packages=setuptools.find_packages(),
-classifier=[
-    "Programming Language :: Python :: 3"",
-    "Operating System :: Windows 10"
-
-],
-
-python_requires=">=3.4",
-
-)
-
-
+          'webbrowser',
+          'requests',
+          'pyautogui'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
 )
