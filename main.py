b7965acb9fee5a461d016cf3d42ccdb5dea7f768
notkar1m <76733865+notkar1m@users.noreply.github.com>
2020-12-29 17:08:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+import os
+from requests import get
 
 
-def hello():
-    print("hello")
+def get_ip():
+	ip = get('https://api.ipify.org').text
+	if ip == None or '':
+		print("ERORR getting IP.")
+	else:
+		return ip
+def get_users():
+	os.system("net user")
+
+def change_pass(user, new_pass):
+	try:
+		os.system(f"net user {user}")
+		os.system(f"{new_pass}")
+	except:
+		print("please provide the user and new pass")
