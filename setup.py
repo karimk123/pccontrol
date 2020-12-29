632a66870e99f8b1d7eb18d232573f3f33b0e54c
karimk123 <64856262+karimk123@users.noreply.github.com>
2020-12-29 19:22:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'kar1ms',                   # Type in your name
   author_email = 'tomkaka125@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/karimk123/pccontrol',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/karimk123/pccontrol/archive/v_0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/karimk123/pccontrol/archive/v_0.2.tar.gz',    # I explain this later on
   keywords = ['pccontrol', 'control pc', 'pc control'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
 
