b7c59cbb1990e532e9bca7b27a8d72071ce58400
karimk123 <64856262+karimk123@users.noreply.github.com>
2020-12-29 18:25:04 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+
+
+setuptools.setup(
+name="pccontroller",
+version="1.0",
+author="kar1ms",
+author_email="tomkaka125@gmail.com",
+description="This is a python library that gives you functions for doing multiple things on pc.",
+url="https://github.com/karimk123/pccontroller",
+packages=setuptools.find_packages(),
+classifier=[
+    "Programming Language :: Python :: 3"",
+    "Operating System :: Windows 10"
+
+],
+
+python_requires=">=3.4",
+
+)
+
+
+)
