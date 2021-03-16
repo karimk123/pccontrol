d29ec7616d5aae22203d28b0c970b56a9b3ef0cd
karimk123 <64856262+karimk123@users.noreply.github.com>
2021-03-16 09:00:44 +0200

    updated version num

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pccontrol',         # How you named your package folder (MyLib)
   packages = ['pccontrol'],   # Chose the same as "name"
-  version = '0.2',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a python library that gives you functions for doing multiple things on pc.',   # Give a short description about your library
   author = 'kar1ms',                   # Type in your name
